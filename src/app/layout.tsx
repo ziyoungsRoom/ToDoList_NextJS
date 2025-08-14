5e68c6777a7c88152a4a340bed345eeed900b5a3
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-15 02:01:48 +0900

    chore: 언어 설정 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={`antialiased`}>{children}</body>
     </html>
   );
