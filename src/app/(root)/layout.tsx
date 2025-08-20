7ad97cd54c246efa76ce1b2640dcd35829c2204d
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-20 18:27:40 +0900

    style: Layout 여백 추가

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -3,9 +3,11 @@ import { PropsWithChildren } from "react";
 
 const RootLayout = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <div className="flex flex-col min-h-screen ">
+    <div className="w-full flex flex-col min-h-screen ">
       <Header />
-      <div className="flex-grow mt-[70px] tablet:mt-[100px]">{children}</div>
+      <div className="flex-grow mt-[70px] tablet:mt-[100px] px-4">
+        {children}
+      </div>
     </div>
   );
 };
