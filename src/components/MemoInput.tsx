774c32553c40ea322ca8cfdbedb131e2aa97a029
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-22 02:09:38 +0900

    feat: Memo 텍스트 추가

--- src/components/MemoInput.tsx
+++ src/components/MemoInput.tsx
@@ -2,8 +2,10 @@ import Memo from "@/assets/Memo.svg";
 
 const MemoInput = () => {
   return (
-    <div className="w-full h-full min-h-[240px] rounded-3xl">
-      <Memo className="w-[100%] h-[100%] min-h-[240px] rounded-3xl" />
+    <div className="relative w-full h-full rounded-3xl flex flex-col items-center justify-center">
+      <Memo className="w-[100%] h-[100%] min-h-[311px] rounded-3xl" />
+      <div className="absolute top-5 text-amber-800 font-extrabold">Memo</div>
+      <div className="absolute">비타민을 먹었는데 오줌이 노란생이야.</div>
     </div>
   );
 };
