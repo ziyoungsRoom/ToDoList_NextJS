ff593597c7cc067e67b10749199cc0f48e6a0ee8
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-21 20:39:24 +0900

    refactor: Empty 간단한 리팩토링

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -9,29 +9,29 @@ interface EmptyProps {
 
 const Empty = ({ section }: EmptyProps) => {
   return (
-    <>
+    <div className="w-full flex flex-col items-center my-[10%]">
       {section === "todo" && (
-        <div className="w-full flex flex-col items-center mt-[20%]">
+        <>
           <EmptyTodoSmall className="tablet:hidden w-[120px] h-[120px]" />
           <EmptyTodoLarge className="hidden tablet:block w-[240px] h-[240px]" />
           <div className="text-center text-slate-400 text-[16px]">
             <div>할 일이 없어요.</div>
             <div>TODO를 새롭게 추가해주세요!</div>
           </div>
-        </div>
+        </>
       )}
 
       {section === "done" && (
-        <div className="w-full flex flex-col items-center mt-[20%]">
+        <>
           <EmptyDoneSmall className="tablet:hidden w-[120px] h-[120px]" />
           <EmptyDoneLarge className="hidden tablet:block w-[240px] h-[240px]" />
           <div className="text-center text-slate-400 text-[16px]">
             <div>아직 다 한 일이 없어요.</div>
             <div>해야 할 일을 체크해보세요!</div>
           </div>
-        </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 
