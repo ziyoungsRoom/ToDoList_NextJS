1fc71ca9bb140854af8dd1871483fa49ca160daf
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-21 14:41:08 +0900

    feat: input 컴포넌트 작성

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -12,7 +12,7 @@ const SearchComponent = ({ value, setValue }: SearchProps) => {
       value={value}
       placeholder="할 일을 입력하세요."
       onChange={onChange}
-      className="w-[80%] h-[64px] px-[24px] py-[17px] text-[16px] outline-none rounded-full bg-slate-100 border-slate-900 border-[2px] shadow-slate-900 shadow-[2px_2px_0] placeholder:text-slate-500"
+      className="w-full h-[64px] px-[24px] py-[17px] text-[16px] outline-none rounded-full bg-slate-100 border-slate-900 border-[2px] shadow-slate-900 shadow-[2px_2px_0] placeholder:text-slate-500"
     />
   );
 };
