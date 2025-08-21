60148577c51d0657f78e1133e061d4b40a7e6581
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-22 07:03:52 +0900

    feat: 상세페이지 check 기능 추가

--- src/components/CheckListDetail.tsx
+++ src/components/CheckListDetail.tsx
@@ -4,11 +4,13 @@ import DoneCheckbox from "@/assets/DoneCheckbox.svg";
 interface CheckListDetailProps {
   text: string;
   isDone: boolean;
+  onClick: React.MouseEventHandler<HTMLElement>;
 }
 
-const CheckListDetail = ({ text, isDone }: CheckListDetailProps) => {
+const CheckListDetail = ({ text, isDone, onClick }: CheckListDetailProps) => {
   return (
     <div
+      onClick={onClick}
       className={`${
         isDone ? "bg-violet-100" : "bg-white"
       } w-full h-[64px] p-[8px] flex items-center justify-center gap-[16px] text-[20px] font-bold cursor-pointer rounded-2xl border border-[2px] border-slate-900`}
