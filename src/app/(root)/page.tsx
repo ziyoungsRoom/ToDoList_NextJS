c058a4cb96bed810124b95bb9972533fc1308e99
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-22 03:02:29 +0900

    feat: state 하나로 관리하도록 수정

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,30 +7,30 @@ import Empty from "@/components/Empty";
 import { useEffect, useState } from "react";
 import SearchComponent from "@/components/SearchComponent";
 import MainButton from "@/components/MainButton";
-import { GetTodos } from "@/api/todoApi";
+import { GetTodos, PostTodo } from "@/api/todoApi";
 import CheckList from "@/components/CheckList";
 import { GetItem } from "@/type/type";
 
 const MainPage = () => {
-  const [todoItems, setTodoItems] = useState<GetItem[]>([]);
-  const [doneItems, setDoneItems] = useState<GetItem[]>([]);
+  const [items, setItems] = useState<GetItem[]>([]);
   const [value, setValue] = useState<string>("");
 
+  // 페이지가 렌더링되면 todolist 를 불러와요.
   useEffect(() => {
     const GetItems = async () => {
       const data: GetItem[] = await GetTodos();
 
       if (!data.length) return;
 
-      const todoItems = data.filter((todo) => todo.isCompleted === false);
-      const doneItems = data.filter((todo) => todo.isCompleted === true);
-
-      if (todoItems.length) setTodoItems([...todoItems]);
-      if (doneItems.length) setDoneItems([...doneItems]);
+      setItems([...data]);
     };
 
     GetItems();
   }, []);
+
+  // isCompleted 값 필터링을 통해 배열을 분리해요.
+  const todoItems = items.filter((todo) => todo.isCompleted === false);
+  const doneItems = items.filter((todo) => todo.isCompleted === true);
 
   return (
     <div className="w-full min-h-screen desktop:w-[80%] mx-auto mt-[20px] px-4 flex flex-col gap-12">
@@ -39,10 +39,10 @@ const MainPage = () => {
         <MainButton variants="Add" state="active" />
       </div>
       <div className="w-full flex flex-col items-center desktop:items-start desktop:flex-row gap-16 ">
-        <section className="w-full min-w-[120px] h-full flex flex-col gap-4 ">
+        <section className="w-full min-w-[120px] h-full min-h-[240px] flex flex-col gap-4 ">
           <Todo />
-          <div className="w-full h-full">
-            {!todoItems.length && <Empty section={"todo"} />}
+          <div className="w-full h-full flex flex-col gap-2">
+            {!todoItems && <Empty section={"todo"} />}
             {todoItems &&
               todoItems.map((todo) => (
                 <CheckList
@@ -54,7 +54,7 @@ const MainPage = () => {
               ))}
           </div>
         </section>
-        <section className="w-full min-w-[120px] h-full flex flex-col gap-4 ">
+        <section className="w-full min-w-[120px] h-full min-h-[240px] flex flex-col gap-4 ">
           <Done />
           <div>
             {!doneItems.length && <Empty section={"done"} />}
