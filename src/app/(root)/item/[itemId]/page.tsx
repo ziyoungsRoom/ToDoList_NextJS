c6176c3db94d9d8105fd0ce2e36690e266ab4f42
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-22 01:35:48 +0900

    style: 배경 색 설정 변경

--- src/app/(root)/item/[itemId]/page.tsx
+++ src/app/(root)/item/[itemId]/page.tsx
@@ -5,15 +5,17 @@ import MemoInput from "@/components/MemoInput";
 
 const ItemPage = () => {
   return (
-    <div className="w-full desktop:w-[80%] min-w-[400px] h-full min-h-screen mx-auto bg-white px-[10%] py-[20px] flex flex-col items-center">
-      <CheckListDetail text="비타민 챙겨먹기!" isDone={false} />
-      <div className="w-full min-h-[311px] flex flex-col desktop:flex-row gap-8 py-4">
-        <ImageInput />
-        <MemoInput />
-      </div>
-      <div className="w-full flex justify-center desktop:justify-end  gap-4">
-        <MainButton variants="Edit" />
-        <MainButton variants="Delete" />
+    <div className="w-full h-full bg-slate-100">
+      <div className="w-full desktop:w-[80%] min-w-[400px] h-full min-h-[calc(100vh-60px)] mx-auto bg-white px-[10%] py-[20px] flex flex-col items-center">
+        <CheckListDetail text="비타민 챙겨먹기!" isDone={false} />
+        <div className="w-full min-h-[311px] flex flex-col desktop:flex-row gap-8 py-4">
+          <ImageInput />
+          <MemoInput />
+        </div>
+        <div className="w-full flex justify-center desktop:justify-end  gap-4">
+          <MainButton variants="Edit" />
+          <MainButton variants="Delete" />
+        </div>
       </div>
     </div>
   );
