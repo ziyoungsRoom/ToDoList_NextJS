83fce62e6a319572113c83265d6b1540ef343a42
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-21 21:26:41 +0900

    style: 상세페이지 UI 작성

--- src/app/(root)/item/[itemId]/page.tsx
+++ src/app/(root)/item/[itemId]/page.tsx
@@ -1,5 +1,22 @@
+import CheckListDetail from "@/components/CheckListDetail";
+import ImageInput from "@/components/ImageInput";
+import MainButton from "@/components/MainButton";
+import MemoInput from "@/components/MemoInput";
+
 const ItemPage = () => {
-  return <div>item 상세페이지</div>;
+  return (
+    <div className="w-full desktop:w-[80%] min-w-[400px] h-full min-h-screen mx-auto bg-white px-[10%] py-[20px] flex flex-col items-center">
+      <CheckListDetail text="비타민 챙겨먹기!" isDone={false} />
+      <div className="w-full min-h-[311px] flex flex-col desktop:flex-row gap-8 py-4">
+        <ImageInput />
+        <MemoInput />
+      </div>
+      <div className="w-full flex justify-center desktop:justify-end  gap-4">
+        <MainButton variants="Edit" />
+        <MainButton variants="Delete" />
+      </div>
+    </div>
+  );
 };
 
 export default ItemPage;
