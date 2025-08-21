e756212d8bcd2ea7d208935c9a2e0b52f03010a2
ziyoungsRoom <ilikeeeyo.o@gmail.com>
2025-08-21 21:25:57 +0900

    del: onClick 함수 삭제

--- src/components/CheckListDetail.tsx
+++ src/components/CheckListDetail.tsx
@@ -4,19 +4,14 @@ import DoneCheckbox from "@/assets/DoneCheckbox.svg";
 interface CheckListDetailProps {
   text: string;
   isDone: boolean;
-  setIsDone: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const CheckListDetail = ({ text, isDone, setIsDone }: CheckListDetailProps) => {
-  const onClick: React.MouseEventHandler<HTMLElement> = () => {
-    setIsDone(!isDone);
-  };
+const CheckListDetail = ({ text, isDone }: CheckListDetailProps) => {
   return (
     <div
-      onClick={onClick}
       className={`${
         isDone ? "bg-violet-100" : "bg-white"
-      } w-[90%] h-[64px] p-[8px] flex items-center justify-center gap-[16px] text-[20px] cursor-pointer rounded-full border border-[2px] border-slate-900`}
+      } w-full h-[64px] p-[8px] flex items-center justify-center gap-[16px] text-[20px] cursor-pointer rounded-full border border-[2px] border-slate-900`}
     >
       {isDone ? <DoneCheckbox /> : <DefaultCheckbox />}
 
